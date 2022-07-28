69e148c89b25d116ebdea889d5138076e155338f
CIFU1199 <edwkamilo@gmail.com>
2022-07-28 18:18:50 -0500

    Guardar Cambios del Cliente a Editar

--- js/API.js
+++ js/API.js
@@ -33,9 +33,38 @@ export const eliminarCliente = async id =>{
     try {
         await fetch(`${url}/${id}`,{
             method: 'DELETE',
-            
+
         });
     } catch (error) {
         console.log(error);
     }
 } 
+
+//obtiene un cliente por su ID 
+
+export const obtenerCliente = async id =>{
+    try {
+        const resultado = await fetch(`${url}/${id}`);
+        const cliente = await resultado.json();
+        return cliente;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//actualiza un registro 
+export const editarCliente = async cliente =>{
+    try {
+        await fetch(`${url}/${cliente.id}`,{
+            method:'PUT',
+            body: JSON.stringify(cliente),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        });
+        window.location.href = 'index.html';
+    } catch (error) {
+        console.log(error);
+    }
+}
